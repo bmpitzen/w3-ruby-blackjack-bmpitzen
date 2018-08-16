9f3f7098a9b57254860c83a0eb343e20303f8710
bmpitzen <bmpitzen@icloud.com>
2018-08-16 16:29:52 -0400

    Initial commit. Added card and deck files.

--- starter_files/blackjack.rb
+++ starter_files/blackjack.rb
@@ -1,3 +1,54 @@
 puts "TODO Implement the game of blackjack."
 
-# Hint: for starters, read bin/blackjack.rb
+require_relative 'card'
+require_relative 'deck'
+
+
+class BlackjackGame
+
+  def initialize
+    deck = Deck.new
+    deck.shuffle
+    
+  end
+
+end
+
+
+
+
+# def hand
+#   while true
+#     puts "Would you like to play? (y)es or (n)o?"
+#     answer = gets.chomp.downcase
+#     if answer[0] == "y"
+#     return true
+#       print "Please enter (f)ronthand or (b)ackhand: "
+#       hand_choice = gets.chomp.downcase
+#       if hand_choice[0] == "f"
+#         puts "FRONTHAND!"
+#       elsif hand_choice[0] == "b"
+#         puts "BACKHAND!"
+#       end
+#     elsif answer[0] == "n"  
+#       return false
+#     end
+#   end
+# end
+
+
+
+
+# def get_yes_or_no
+#   # loop until you get a good answer and return
+#   while true
+#     print "Please enter (y)es or (n)o: "
+#     answer = gets.chomp.downcase
+#     if answer[0] == "y"
+#       return true
+#     elsif answer[0] == "n"
+#       return false
+#     end
+#     puts "That is not a valid answer!"
+#   end
+# end
---
+++ starter_files/card.rb
@@ -0,0 +1,20 @@
+class Card
+
+attr_accessor :rank, :suit
+
+def initialize ( rank, suit )
+  @rank = rank
+  @suit = suit
+end
+
+def greater_than? (card)
+order = [:A,1,2,3,4,5,6,7,8,9,10,:J,:Q,:K]
+order.index(@rank)>order.index(card.rank)
+end
+
+def ==(another_card)
+  self.suit == another_card.suit and
+  self.rank == another_card.rank
+end
+
+end
